02cff0c72dab578409154b529a048a696a2a4cd0
Haard30 <36398394+Haard30@users.noreply.github.com>
2018-04-11 19:20:16 +0530

    docs updated

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+	<h1>Documentation</h1><br><br>
+<a href="getAllUserPaginationDoc.html"> Pagination API</a><br><br>
+
+<a href="getUserPeopleRatingDoc.html"> Get User People Rating API</a><br><br>
+<a href="getUserTechRatingDoc.html"> Get User Tech Rating API</a><br><br>
+<a href="getUserStatusDoc.html"> Get User Status API</a><br><br>
+<a href="getUserRoleDoc.html"> Get User Role API</a><br><br>
+<a href="getUserDoc.html"> Get User Created After API</a><br><br>
+<a href="getUserHashTagsDoc.html"> Get User Hashtag API</a><br><br>
+<a href="updatePeopleRatingDoc.html"> Update People Rating API</a><br><br>
+<a href="updateTechRatingDoc.html"> Update Tech Rating API</a><br><br>
+<a href="bulkUpdateuserDoc.html"> Bulk Update User API</a><br><br>
+<a href="updateStatusDoc.html"> Update Status API</a><br><br>
+<a href="updateUserHashTagsDoc.html"> Update User Hashtags API</a><br><br>
+<a href="updateUserRoleDoc.html"> Update User role API</a><br><br>
+</body>
+</html>
